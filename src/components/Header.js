5f6a16a3e2ee15b40165aa322155382e3725eae7
ain88 <ahyungss@hotmail.com>
2020-07-26 23:37:07 -0700

    fix the typo

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const handleClick = () => {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ml-auto">
               <NavLink href="tovolunteer">Volunteer Today</NavLink>
-              <NavLink href="needvolunteer">Be Organizer</NavLink>
+              <NavLink href="needvolunteer">Be an Organizer</NavLink>
               {
                 props.loggedInStatus ?
                   <NavLink href="/" onClick={handleClick}>Log Out</NavLink>:
--- src/components/Needvolunteer.js
+++ src/components/Needvolunteer.js
@@ -1,10 +1,38 @@
 import React from "react"
+import { Form, Button } from 'react-bootstrap'
+
 
 class Needvolunteer extends React.Component {
   render () {
     return (
-      <div className="container content center">
-        <h3>Need volunteers? Be an Organizer!</h3>
+      <div className="container content">
+        <h3 className="center">Need volunteers? Be an Organizer!</h3>
+        <br />
+
+        <Form>
+          <Form.Group controlId="theType">
+            <Form.Label>Select type of volunteer</Form.Label>
+            <Form.Control as="select">
+              <option>One time tasks</option>
+              <option>Material need</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="theTitle">
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group controlId="theDescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+          <Form.Group controlId="theLocation">
+            <Form.Label>Location</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+          Submit
+          </Button>
+        </Form>
       </div>
     );
   }
