ba4811062d9da079e2f02386d88c3071a22182c7
ain88 <ahyungss@hotmail.com>
2020-08-01 19:36:43 -0700

    worked on request page

--- src/components/Needvolunteer.js
+++ src/components/Needvolunteer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Form, Button } from 'react-bootstrap'
 import PlacesAutocomplete, {
   geocodeByAddress,
+  geocodeByPlaceId,
   getLatLng,
 } from 'react-places-autocomplete';
 import axios from 'axios'
@@ -13,11 +14,35 @@ class Needvolunteer extends React.Component {
       address: '',
       address2: '',
       title: '',
-      type: '',
+      typev: '',
       description: '',
+      address_lat: '',
+      address_lng: '',
+      user_id: '',
       errors: ''
     };
   }
+  //
+  // componentDidMount(){
+  //   axios.get('http://localhost:3001/logged_in')
+  //   .then(function (response) {
+  //   console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
+  // }
+  // 
+  // componentDidMount(){
+  //   if(this.props.isLoggedIn == false){
+  //     console.log("not logged in")
+  //   }else {
+  //     this.setState({
+  //       user_id: this.props.user
+  //     })
+  //     console.log(user_id)
+  //   }
+  // }
 
   handleChange = address => {
     this.setState({ address });
@@ -32,42 +57,56 @@ class Needvolunteer extends React.Component {
 
   handleSelect = address => {
     this.setState({ address })
-    const results = geocodeByAddress(address);
-    const latLng = getLatLng(results[0]);
-    this.setState({ address2: latLng });
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng =>
+        this.setState({
+          address2: latLng.lat + "," + latLng.lng
+        })
+      )
+      .catch(error => console.error('Error', error));
+
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const request = {
-      type: this.state.type,
-      title: this.state.title,
-      description: this.state.description,
-      location: this.state.address2
-    };
 
-    axios.post('http://localhost:3001/requests', { request })
-      .then(res => {
-        console.log(res);
-        console.log(res.data);
+    fetch('http://localhost:3001/requests', {
+      method: 'POST',
+      headers:  {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },withCredentials: true,
+      body: JSON.stringify({
+        typev: this.state.typev,
+        title: this.state.title,
+        description: this.state.description,
+        location: this.state.address2,
+        owner: this.state.user_id
       })
-        .catch(err => {
-        console.log(err);
-      })
-  }
-
-  handleErrors = () => {
-    return (
-      <div>
-        <ul>{this.state.errors.map((error) => {
-          return <li key={error}>{error}</li>
-        })}</ul>
-      </div>
-    )
-  }
+    }).then(response => response.json()).then(
+      data => {
+            if (data.status === 'created') {
+            } else {
+              this.setState({
+                errors: data.errors
+              })
+        }}
+      )
+    .catch(error=>console.log(error));
+  };
+    handleErrors = () => {
+      return (
+        <div>
+          <ul>{this.state.errors.map((error) => {
+            return <li key={error}>{error}</li>
+          })}</ul>
+        </div>
+      )
+    }
 
   render() {
-    const {type, title, description, address, address2, errors} = this.state;
+    const {typev, title, description, address, address2, user_id, errors} = this.state;
     return (
       <div className="container content">
   <h3 className="center">Need volunteers? Be an Organizer!</h3>
@@ -75,11 +114,12 @@ class Needvolunteer extends React.Component {
 
   <Form onSubmit={this.handleSubmit}>
     <Form.Group controlId="theType">
-      <Form.Label>Select type of volunteer</Form.Label>
-      <Form.Control as="select" name="type" value={type} onChange={this.handleChange2}>
-        <option>One time tasks</option>
-        <option>Material need</option>
-      </Form.Control>
+    <Form.Label>Select type of volunteer</Form.Label>
+    <Form.Control as="select" name="typev" value={typev} onChange={this.handleChange2}>
+    <option value="">Select</option>
+    <option value="1">One time tasks</option>
+    <option value="2">Material need</option>
+    </Form.Control>
     </Form.Group>
     <Form.Group controlId="theTitle">
       <Form.Label>Title</Form.Label>
@@ -136,7 +176,11 @@ class Needvolunteer extends React.Component {
     Submit
     </Button>
   </Form>
-
+  <div>
+   {
+     this.state.errors ? this.handleErrors() : null
+   }
+ </div>
 </div>
     );
   }
